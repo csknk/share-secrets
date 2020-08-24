6c49fdc7a77144606c2738a3d4864c205013b631
David Egan <david@carawebs.com>
2020-08-24 12:04:25 +0100

    Basic documentation.

--- CreateFiles.py
+++ CreateFiles.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 import pathlib
 from pathlib import Path
 import os
@@ -24,11 +23,27 @@ class CreateFiles():
         self.clean_up()
 
     def load_config(self):
+        """Load config data
+
+        Config data in config.json should include the following fields:
+
+        {
+          "contact": {
+            "name": "Joe Bloggs",
+            "email": "joe@example.com"
+          },
+          "fragments": {
+            "filenameRoot": "JB-secret"
+          }
+        }
+        """
         with open('config.json') as f:
             self.config = json.load(f)
 
-
     def set_base_dir(self):
+        """Obtain the base directory from user input.
+        Default directory is /tmp
+        """
         while 1:
             msg = "Please enter a path: blank defaults to /tmp: "
             path = Path(input(msg) or "/tmp").expanduser()
@@ -38,6 +53,7 @@ class CreateFiles():
         self.base_dir = path
     
     def set_base_dir_graphical(self):
+        """Probably deprecated."""
         cmd = [
             'zenity',
             '--file-selection',
@@ -50,8 +66,12 @@ class CreateFiles():
         except subprocess._called_process_error as e:
             print(e.output)
 
-    # Output fragments to files
     def save_to_files(self):
+        """
+        Setup directory structure and control file creation for each fragment in the shares_list.
+        
+        Loop through the shares_list and create markdown, HTML & PDF files for each.
+        """
         subdir = "shared-secrets-" + str(int(time.time()))
         self.dir = self.base_dir / subdir
         pathlib.Path(self.dir).mkdir(parents=True, exist_ok=True, mode=0o755)
@@ -70,11 +90,16 @@ class CreateFiles():
             self.create_file(fragment=fragment, dirpath=self.dir, filename=filename, filetype="pdf")
 
     def write_file(self, filepath, content):
+        """Write content into file specified by filepath.""" 
         file = open(filepath, 'w')
         file.write(content)
         file.close()
  
     def create_file(self, filetype, **kwargs):
+        """Create markdown, HTML and PDF files.
+        
+        Note that PDF requires a HTML input.
+        """
         if kwargs:
             fragment = kwargs["fragment"]
             dirpath = kwargs["dirpath"]
@@ -115,7 +140,8 @@ class CreateFiles():
         else:
             self.write_file(filepath, content)
 
-        def make_qr_code(self, fragment):
+    def make_qr_code(self, fragment):
+        """Makes a QR Code from a string - the secret fragment."""
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -127,9 +153,13 @@ class CreateFiles():
         return qr.make_image(fill_color="black", back_color="white")
 
     def create_readme(self, dir):
+        """Create a document with user instructions.i
+            
+            Parameters: Root directory of the generated fragment files.
+        """
         d = {
-        'label': self.label,
-        'timestamp': datetime.datetime.now().strftime("%d-%b-%Y %H:%M:%S")
+            'label': self.label,
+            'timestamp': datetime.datetime.now().strftime("%d-%b-%Y %H:%M:%S")
         }
         filein = open("text/readme.txt")
         src = Template(filein.read())
@@ -140,12 +170,18 @@ class CreateFiles():
         file.close()
 
     def clean_up(self):
+        """Securely delete sensitive material.
+
+        Uses GNU shred utility (https://www.gnu.org/software/coreutils/manual/html_node/shred-invocation.html)
+        This should be available on TAILS.
+        """
         print("Your secrets have been split and saved as individual files. Holding these files in one place is a security vulnerability.")
         print("Files:")
         pathlist = Path(self.dir).glob('**/*')
         for path in pathlist:
             print(path)
         if click.confirm("Do you want to securely shred the files?", default=True):
+            # TODO: References incorrect file paths - fix this. Should shred all png, pdf, html, md
             pathlist = Path(self.dir).glob('**/*.txt')
             for index, path in enumerate(pathlist):
                 print("_shredding {}...".format(str(path)))
